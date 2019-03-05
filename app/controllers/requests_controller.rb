cc9feed5fb8122cdbd01e5e6ea5cc3e02f65f11e
sylvain wehbe <sylvain.w@icloud.com>
2019-03-05 18:12:31 +0100

    Request Creation Form 1/2

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,26 @@
+class RequestsController < ApplicationController
+  def new
+    @request = Request.new
+  end
+
+  def create
+    @request = Request.new(request_params)
+    @request.user = current_user
+    if @request.save
+      redirect_to edit_request_path(@request.user)
+    else
+      p @request.errors.messages
+      render 'new'
+    end
+  end
+
+  def edit
+    @request = Request.find(params[:id])
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:price, :meal_time, :location, :meeting_date)
+  end
+end
