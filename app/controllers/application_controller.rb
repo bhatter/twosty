8ac54106f9cea31f562819d451201074049715c6
Mahesh Bhatter <maheshbhatter@gmail.com>
2019-03-06 11:19:37 +0100

    gender added to users model controller and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :date_of_birth, :gender_preference, :age_preference_from, :age_preference_to, :photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :date_of_birth, :gender, :gender_preference, :age_preference_from, :age_preference_to, :photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name, :date_of_birth, :gender_preference, :age_preference_from, :age_preference_to, :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name, :date_of_birth, :gender, :gender_preference, :age_preference_from, :age_preference_to, :photo])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_05_141410) do
+ActiveRecord::Schema.define(version: 2019_03_06_095924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2019_03_05_141410) do
     t.integer "age_preference_from"
     t.integer "age_preference_to"
     t.string "photo"
+    t.integer "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
