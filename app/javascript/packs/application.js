f70b631280667aed1db8188dda4b28043e8c8e2e
sylvain wehbe <sylvain.w@icloud.com>
2019-03-08 13:47:29 +0100

    Navbar, Sidebar and JS animation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,8 @@ import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the styleshe
 
 import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
+import { sidebarToggle } from '../components/navbar';
 
 initMapbox();
 initAutocomplete();
+sidebarToggle();
