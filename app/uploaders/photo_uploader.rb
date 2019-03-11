1b81f70abe9273213560a3da5011759d9ee3f468
Mahesh Bhatter <maheshbhatter@gmail.com>
2019-03-11 14:04:11 +0100

    finished index and fixes

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -11,7 +11,12 @@ class PhotoUploader < CarrierWave::Uploader::Base
   end
 
   version :find_face do
-    cloudinary_transformation radius: 20,
+    cloudinary_transformation radius: 50,
       width: 150, height: 150, crop: :thumb, gravity: :face
+  end
+
+  version :find_face_small do
+    cloudinary_transformation radius: 50,
+      width: 100, height: 100, crop: :thumb, gravity: :face
   end
 end
