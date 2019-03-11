051bf200554d4dca73e138f7ace5130409efdaf9
sylvain wehbe <sylvain.w@icloud.com>
2019-03-11 15:59:10 +0100

    HomePage and RequestNew Recfacto.

--- app/javascript/components/scroll.js
+++ app/javascript/components/scroll.js
@@ -1,26 +1,27 @@
 const scroll = () => {
   const arrowsDown = document.querySelectorAll(".scroll_down_btn");
+  const arrowsUp = document.querySelectorAll(".scroll_up_btn");
   const sections = document.querySelectorAll(".section");
-  let conteur = 0;
+  let counter = 0;
 
   arrowsDown.forEach((arrowDown,index) => {
     arrowDown.addEventListener("click", event => {
-      conteur = conteur - 100
-      sections[index+1].style.transform = "translateY(" + conteur + "vh)";
+      console.log(counter);
+      counter = counter - 100;
+      sections[index+1].style.transform = "translateY(" + counter + "vh)";
       sections[index+1].style.opacity = 1;
       sections[index].style.opacity = 0;
     });
   });
 
-  const arrowsUp = document.querySelectorAll(".scroll_up_btn");
-
   arrowsUp.forEach((arrowUp,index) => {
     arrowUp.addEventListener("click", event => {
-      conteur = conteur + 100
-      sections[index+1].style.transform = "translateY(" + conteur + "vh)";
+      counter = counter + 100;
+      sections[index+1].style.transform = "translateY(" + counter + "vh)";
       sections[index].style.opacity = 1;
       sections[index+1].style.opacity = 0;
     });
   });
+};
 
-  export { scroll };
+export { scroll };
