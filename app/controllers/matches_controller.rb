4357d5dbef33752bf1260fec5f887c7ab5901169
jason kim <jkim9348@gmail.com>
2019-03-13 09:55:53 +0100

    fixed match index, doesnt show requests without restaurants

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -13,7 +13,9 @@ class MatchesController < ApplicationController
   private
 
   def match_requests(request)
-    requests = Request.where(location: request.location, meeting_date: request.meeting_date, cuisine: request.cuisine, meeting_id: nil).where.not(user: current_user)
+    parsed_requests = []
+    requests = Request.where(location: request.location, meeting_date: request.meeting_date, cuisine: request.cuisine, meeting_id: nil).where.not(user: current_user).
+      where.not(restaurant: [nil, ""])
     requests.joins(:user)
             .where(users: { gender: current_user.preferred_gender, gender_preference: current_user.gender_to_preferred })
             .where(users: { date_of_birth: current_user.birth_date_preference_to..current_user.birth_date_preference_from})
